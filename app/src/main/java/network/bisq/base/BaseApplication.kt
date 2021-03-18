68cbe4ce395a61839bcb199c63cd554bc38afe11
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 15:43:37 -0300

    Adding modules to koin

--- app/src/main/java/network/bisq/base/BaseApplication.kt
+++ app/src/main/java/network/bisq/base/BaseApplication.kt
@@ -1,10 +1,18 @@
 package network.bisq.base
 
 import android.app.Application
+import network.bisq.module.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
+@Suppress("UNUSED")
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
+
+        startKoin {
+            androidContext(this@BaseApplication)
+            modules(listOf(appModule))
+        }
     }
 }
