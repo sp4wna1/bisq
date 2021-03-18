8a198a2a022a8825571d79265890183511aefbbb
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 13:00:11 -0300

    Adding viewmodel

--- markets/src/main/java/bisq/markets/view/MarketsFragment.kt
+++ markets/src/main/java/bisq/markets/view/MarketsFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import bisq.markets.databinding.FragmentMarketsBinding
+import bisq.markets.viewmodel.MarketsViewModel
 import network.bisq.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MarketsFragment : BaseFragment() {
 
     private lateinit var binding: FragmentMarketsBinding
+
+    private val viewModel: MarketsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
