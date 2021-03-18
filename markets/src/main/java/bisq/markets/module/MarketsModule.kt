9f9f4405a022e7ea1caf3124e88165a17b9686cc
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 15:43:22 -0300

    Updating injections

--- markets/src/main/java/bisq/markets/module/MarketsModule.kt
+++ markets/src/main/java/bisq/markets/module/MarketsModule.kt
@@ -1,9 +1,22 @@
 package bisq.markets.module
 
+import bisq.markets.repository.DepthRepository
+import bisq.markets.repository.DepthRepositoryImpl
+import bisq.markets.service.DepthService
+import bisq.markets.usecase.DepthUseCase
+import bisq.markets.usecase.DepthUseCaseImpl
 import bisq.markets.viewmodel.MarketsViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
+@InternalCoroutinesApi
 val marketsModule = module {
-    viewModel { MarketsViewModel() }
+    viewModel { MarketsViewModel(get()) }
+
+    factory<DepthUseCase> { DepthUseCaseImpl(get()) }
+    factory<DepthRepository> { DepthRepositoryImpl(get()) }
+    factory<DepthService> { get<Retrofit>(named("RETROFIT")).create(DepthService::class.java) }
 }
