43d471d950abd1735b1e30f6356b0b5cb17ca573
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 15:43:07 -0300

    Calling usecase

--- markets/src/main/java/bisq/markets/viewmodel/MarketsViewModel.kt
+++ markets/src/main/java/bisq/markets/viewmodel/MarketsViewModel.kt
@@ -1,7 +1,34 @@
 package bisq.markets.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import bisq.local.BidsAndAsks
+import bisq.local.Resource
+import bisq.markets.usecase.DepthUseCase
+import bisq.markets.usecase.presenter.Presenter
+import kotlinx.coroutines.launch
 import network.bisq.base.BaseViewModel
 
-internal class MarketsViewModel : BaseViewModel() {
+internal class MarketsViewModel(private val depthUseCase: DepthUseCase) : BaseViewModel() {
 
+    internal val depth = MutableLiveData<Resource<BidsAndAsks>>()
+
+    fun fetchDepth(market: String) {
+        viewModelScope.launch {
+            depthUseCase.fetchDepth(market, object :
+                Presenter<BidsAndAsks> {
+                override fun loading() {
+                    depth.postValue(Resource.loading())
+                }
+
+                override fun error() {
+                    depth.postValue(Resource.error())
+                }
+
+                override fun success(result: BidsAndAsks) {
+                    depth.postValue(Resource.success(result))
+                }
+            })
+        }
+    }
 }
