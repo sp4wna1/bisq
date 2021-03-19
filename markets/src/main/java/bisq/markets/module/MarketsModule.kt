4def188abb1bfcfb081dc0b7fc45b31789b6fe59
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 22:50:31 -0300

    Adding new dependencies to injection

--- markets/src/main/java/bisq/markets/module/MarketsModule.kt
+++ markets/src/main/java/bisq/markets/module/MarketsModule.kt
@@ -2,9 +2,14 @@ package bisq.markets.module
 
 import bisq.markets.repository.DepthRepository
 import bisq.markets.repository.DepthRepositoryImpl
+import bisq.markets.repository.OffersRepository
+import bisq.markets.repository.OffersRepositoryImpl
 import bisq.markets.service.DepthService
+import bisq.markets.service.OffersService
 import bisq.markets.usecase.DepthUseCase
 import bisq.markets.usecase.DepthUseCaseImpl
+import bisq.markets.usecase.OffersUseCase
+import bisq.markets.usecase.OffersUseCaseImpl
 import bisq.markets.viewmodel.MarketsViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -17,6 +22,11 @@ val marketsModule = module {
     viewModel { MarketsViewModel(get()) }
 
     factory<DepthUseCase> { DepthUseCaseImpl(get()) }
+    factory<OffersUseCase> { OffersUseCaseImpl(get()) }
+
     factory<DepthRepository> { DepthRepositoryImpl(get()) }
+    factory<OffersRepository> { OffersRepositoryImpl(get()) }
+
     factory<DepthService> { get<Retrofit>(named("RETROFIT")).create(DepthService::class.java) }
+    factory<OffersService> { get<Retrofit>(named("RETROFIT")).create(OffersService::class.java) }
 }
