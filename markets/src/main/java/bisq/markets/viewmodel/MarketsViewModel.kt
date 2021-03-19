018af1e9e9902b2734d2e66d083c0eda46f123d9
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 22:50:57 -0300

    Using fetchOffers

--- markets/src/main/java/bisq/markets/viewmodel/MarketsViewModel.kt
+++ markets/src/main/java/bisq/markets/viewmodel/MarketsViewModel.kt
@@ -2,31 +2,32 @@ package bisq.markets.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import bisq.local.BidsAndAsks
+import bisq.local.Offer
 import bisq.local.Resource
-import bisq.markets.usecase.DepthUseCase
+import bisq.markets.usecase.OffersUseCase
 import bisq.markets.usecase.presenter.Presenter
 import kotlinx.coroutines.launch
 import network.bisq.base.BaseViewModel
 
-internal class MarketsViewModel(private val depthUseCase: DepthUseCase) : BaseViewModel() {
+internal class MarketsViewModel(
+    private val offersUseCase: OffersUseCase
+) : BaseViewModel() {
 
-    internal val depth = MutableLiveData<Resource<BidsAndAsks>>()
+    internal val offers = MutableLiveData<Resource<Pair<List<Offer>, List<Offer>>>>()
 
-    fun fetchDepth(market: String) {
+    fun fetchOffers(market: String) {
         viewModelScope.launch {
-            depthUseCase.fetchDepth(market, object :
-                Presenter<BidsAndAsks> {
+            offersUseCase.fetchOffers(market, object : Presenter<Pair<List<Offer>, List<Offer>>> {
                 override fun loading() {
-                    depth.postValue(Resource.loading())
+                    offers.postValue(Resource.loading())
                 }
 
                 override fun error() {
-                    depth.postValue(Resource.error())
+                    offers.postValue(Resource.error())
                 }
 
-                override fun success(result: BidsAndAsks) {
-                    depth.postValue(Resource.success(result))
+                override fun success(result: Pair<List<Offer>, List<Offer>>) {
+                    offers.postValue(Resource.success(result))
                 }
             })
         }
