b1351642680ceefadb8cb364ae43709d23646a25
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-18 22:50:02 -0300

    Removing extra line

--- markets/src/main/java/bisq/markets/service/model/DepthResponse.kt
+++ markets/src/main/java/bisq/markets/service/model/DepthResponse.kt
@@ -2,7 +2,6 @@ package bisq.markets.service.model
 
 import com.google.gson.annotations.SerializedName
 
-
 data class DepthResponse(
     @SerializedName("btc_brl")
     val bidsAndAsksResponse: BidsAndAsksResponse?
