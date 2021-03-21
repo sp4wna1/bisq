14b94820ac90676d63fe7a405323fac000c65a2b
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-21 19:54:01 -0300

    Changing to api service

--- markets/src/main/java/bisq/markets/repository/OffersRepositoryImpl.kt
+++ markets/src/main/java/bisq/markets/repository/OffersRepositoryImpl.kt
@@ -1,14 +1,15 @@
 package bisq.markets.repository
 
-import bisq.markets.service.OffersService
+import bisq.markets.service.ApiService
 import bisq.markets.service.model.OffersResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-internal class OffersRepositoryImpl(private val offersService: OffersService) : OffersRepository {
+internal class OffersRepositoryImpl(private val apiService: ApiService) :
+    OffersRepository {
 
     override suspend fun fetchOffers(market: String): Flow<OffersResponse> =
-        flow { emit(offersService.getOffers(market)) }.flowOn(Dispatchers.IO)
+        flow { emit(apiService.getOffers(market)) }.flowOn(Dispatchers.IO)
 }
