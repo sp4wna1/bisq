40a9e62167a31d8fd83670d7a16c402fd5b04745
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-21 14:02:23 -0300

    Creating Splash Fragment

---
+++ app/src/main/java/network/bisq/view/SplashFragment.kt
@@ -0,0 +1,30 @@
+package network.bisq.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import network.bisq.R
+import network.bisq.base.BaseFragment
+import network.bisq.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = FragmentSplashBinding.inflate(inflater, container, false).apply {
+
+        binding = this
+
+        binding.root.postDelayed({
+            binding.root.findNavController().navigate(R.id.action_splashFragment_to_marketsFragment)
+        }, 2000L)
+
+    }.root
+
+}
