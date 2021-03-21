786a7299faf320eebd2f63050d404afa24b77d9d
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2021-03-21 19:55:12 -0300

    Listening to currencies and using adapters clear method

--- markets/src/main/java/bisq/markets/view/MarketsFragment.kt
+++ markets/src/main/java/bisq/markets/view/MarketsFragment.kt
@@ -2,15 +2,18 @@ package bisq.markets.view
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.lifecycle.Observer
 import bisq.local.Resource
 import bisq.markets.databinding.FragmentMarketsBinding
 import bisq.markets.module.marketsModule
 import bisq.markets.view.adapter.AsksAdapter
 import bisq.markets.view.adapter.BidsAdapter
+import bisq.markets.view.adapter.CurrencyAdapter
 import bisq.markets.viewmodel.MarketsViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
 import network.bisq.base.BaseFragment
@@ -46,14 +49,47 @@ class MarketsFragment : BaseFragment() {
 
         initViews()
         observeDepth()
+        observeCurrencies()
 
         // TODO Remove this
-        viewModel.fetchOffers("btc_brl")
+        viewModel.fetchCurrencies()
     }.root
+
 
     private fun initViews() {
         binding.bids.adapter = BidsAdapter()
         binding.asks.adapter = AsksAdapter()
+        binding.currencies.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val currency = (binding.currencies.adapter as CurrencyAdapter).getItem(position)
+                Log.d("GDP", "OnItemSelected btc_${currency.code.toLowerCase()}")
+                viewModel.fetchOffers("btc_${currency.code.toLowerCase()}")
+            }
+        }
+    }
+
+    private fun observeCurrencies() {
+        viewModel.currencies.observe(viewLifecycleOwner, Observer {
+            when (it.status) {
+                Resource.Status.LOADING -> {
+                }
+                Resource.Status.ERROR -> {
+                }
+                Resource.Status.SUCCESS -> {
+                    Log.d("GDP", "${it.data}")
+                    binding.currencies.adapter = CurrencyAdapter(it.data ?: emptyList())
+                }
+            }
+        })
     }
 
     private fun observeDepth() {
@@ -66,10 +102,15 @@ class MarketsFragment : BaseFragment() {
                 Resource.Status.ERROR -> {
                     binding.bidsProgress.visibility = View.GONE
                     binding.asksProgress.visibility = View.GONE
+
+                    (binding.bids.adapter as BidsAdapter).clear()
+                    (binding.asks.adapter as AsksAdapter).clear()
+
                 }
                 Resource.Status.SUCCESS -> {
                     binding.bidsProgress.visibility = View.GONE
                     binding.asksProgress.visibility = View.GONE
+                    Log.d("GDP", "SUCCESS - ${it.data}")
                     (binding.bids.adapter as BidsAdapter).setData(it.data!!.first.filter {
                         it.direction == "BUY"
                     })
