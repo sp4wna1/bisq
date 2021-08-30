52209f1393d9dcdf6bd0ee73998f866742b914e2
Gabriel Prado <gaa.prado@hotmail.com>
2021-08-29 21:16:38 -0300

    Moving to remote module

--- remote/src/main/java/br/com/elitma/remote/RemoteModule.kt
+++ remote/src/main/java/br/com/elitma/remote/RemoteModule.kt
@@ -1,5 +1,6 @@
-package network.bisq.module
+package br.com.elitma.remote
 
+import com.google.gson.GsonBuilder
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -8,12 +9,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 // TODO Move this to properties
 private const val BASE_URL = "https://bisq.markets/"
 
-val appModule = module {
+val remoteModule = module {
+
+    single(named("GSON")) {
+        GsonBuilder().registerTypeAdapter(CurrenciesResponse::class.java, CurrenciesTypeAdapter())
+            .create()
+    }
+
     single(named("RETROFIT")) {
         Retrofit
             .Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(get(named("GSON"))))
             .build()
     }
 }
