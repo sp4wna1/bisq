c8cbc0373ce81f878ae681c62e0d7b2750ceb697
Gabriel Prado <gaa.prado@hotmail.com>
2021-09-06 23:20:48 -0300

    Create AssetViewModel.kt

---
+++ markets/src/main/java/bisq/markets/viewmodel/AssetViewModel.kt
@@ -0,0 +1,32 @@
+package bisq.markets.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import bisq.local.Currency
+import bisq.local.Resource
+import bisq.markets.usecase.CurrenciesUseCase
+import br.com.elitma.remote.MarketsType
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import network.bisq.base.BaseViewModel
+
+internal class AssetViewModel(
+    private val currenciesUseCase: CurrenciesUseCase,
+) : BaseViewModel() {
+
+    internal val currencies = MutableLiveData<Resource<List<Currency>>>()
+
+    fun fetchCurrencies() {
+        viewModelScope.launch {
+            currenciesUseCase.fetchCoins(MarketsType.CRYPTO.name).onStart {
+                currencies.postValue(Resource.loading())
+            }.catch {
+                currencies.postValue(Resource.error(it))
+            }.collect {
+                currencies.postValue(Resource.success(it))
+            }
+        }
+    }
+}
