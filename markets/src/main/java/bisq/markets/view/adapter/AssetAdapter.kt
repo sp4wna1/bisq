b11214a4d1842183f60af6e7ecf3e3a1114ac93a
Gabriel Prado <gaa.prado@hotmail.com>
2021-09-06 21:46:12 -0300

    Create AssetAdapter.kt

---
+++ markets/src/main/java/bisq/markets/view/adapter/AssetAdapter.kt
@@ -0,0 +1,23 @@
+package bisq.markets.view.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import bisq.local.Currency
+
+class AssetAdapter(
+    private val assets: List<Currency>,
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
