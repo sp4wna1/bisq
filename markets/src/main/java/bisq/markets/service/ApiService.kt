2369f9ad1b2e5b6a04656acca21c18a6e9add051
Gabriel Prado <gaa.prado@hotmail.com>
2021-09-06 23:22:27 -0300

    Reformat code

--- markets/src/main/java/bisq/markets/service/ApiService.kt
+++ markets/src/main/java/bisq/markets/service/ApiService.kt
@@ -2,21 +2,29 @@ package bisq.markets.service
 
 import bisq.markets.service.model.DepthResponse
 import bisq.markets.service.model.OffersResponse
-import br.com.elitma.remote.TradesResponse
 import br.com.elitma.remote.CurrenciesResponse
+import br.com.elitma.remote.TradesResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/depth")
-    suspend fun getMarket(@Query("market") market: String): DepthResponse
+    suspend fun getMarket(
+        @Query("market") market: String,
+    ): DepthResponse
 
     @GET("api/offers")
-    suspend fun getOffers(@Query("market") market: String): OffersResponse
+    suspend fun getOffers(
+        @Query("market") market: String,
+    ): OffersResponse
 
     @GET("api/currencies")
-    suspend fun getCurrencies(): CurrenciesResponse
+    suspend fun getCurrencies(
+        @Query("type") type: String?,
+    ): CurrenciesResponse
 
     @GET("bisq/api/markets/trades")
-    suspend fun getTrades(@Query("market") market: String): TradesResponse
+    suspend fun getTrades(
+        @Query("market") market: String,
+    ): TradesResponse
 }
