46e806782e47ba3ad181c9b058b7918951406844
Gabriel Prado <gaa.prado@hotmail.com>
2021-09-06 23:21:38 -0300

    Updating AssetAdapter/AssetFragment

--- markets/src/main/java/bisq/markets/view/AssetFragment.kt
+++ markets/src/main/java/bisq/markets/view/AssetFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import bisq.local.Resource
 import bisq.markets.databinding.FragmentAssetBinding
 import bisq.markets.view.adapter.AssetAdapter
+import bisq.markets.viewmodel.AssetViewModel
 import network.bisq.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AssetFragment : BaseFragment() {
 
     private lateinit var binding: FragmentAssetBinding
+
+    private val viewModel: AssetViewModel by viewModel()
+
+    private val adapter = AssetAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,14 +27,18 @@ class AssetFragment : BaseFragment() {
         savedInstanceState: Bundle?,
     ): View = FragmentAssetBinding.inflate(inflater, container, false).apply {
         binding = this
-
-        initViews()
-
     }.root
 
-    private fun initViews() {
-        binding.coinsList.adapter = AssetAdapter(emptyList())
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeCurrencies()
+    }
 
+    private fun initViews() {
+        viewModel.fetchCurrencies()
+
+        binding.coinsList.adapter = adapter
         binding.searchAsset.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 TODO("Not yet implemented")
@@ -36,5 +48,23 @@ class AssetFragment : BaseFragment() {
                 TODO("Not yet implemented")
             }
         })
+    }
+
+    private fun observeCurrencies() {
+        viewModel.currencies.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Resource.Status.SUCCESS -> {
+                    binding.progress.visibility = View.GONE
+                    adapter.setData(it.data ?: emptyList())
+                }
+                Resource.Status.ERROR -> {
+                    binding.progress.visibility = View.GONE
+                    Toast.makeText(requireContext(), it.error.toString(), Toast.LENGTH_LONG).show()
+                }
+                Resource.Status.LOADING -> {
+                    binding.progress.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
--- markets/src/main/java/bisq/markets/view/adapter/AssetAdapter.kt
+++ markets/src/main/java/bisq/markets/view/adapter/AssetAdapter.kt
@@ -1,23 +1,41 @@
 package bisq.markets.view.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import bisq.local.Currency
+import bisq.markets.databinding.ItemAssetBinding
 
-class AssetAdapter(
-    private val assets: List<Currency>,
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+internal class AssetAdapter : RecyclerView.Adapter<AssetAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    private val assets = mutableListOf<Currency>()
+
+    fun setData(assets: List<Currency>) {
+        this.assets.clear()
+        this.assets.addAll(assets)
+        this.notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = assets.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ItemAssetBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setUp(assets[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    internal class ViewHolder(
+        private val binding: ItemAssetBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
 
+
+        fun setUp(currency: Currency) {
+            binding.name.text = currency.name
+
+            binding.root.setOnClickListener {
+            }
+        }
+
+    }
 }
