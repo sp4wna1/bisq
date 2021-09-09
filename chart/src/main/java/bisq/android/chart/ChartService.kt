f433951d2b79113e62fbfee286b5ff15b41850f3
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-09 18:32:18 -0300

    Update ChartService.kt

--- chart/src/main/java/bisq/android/chart/ChartService.kt
+++ chart/src/main/java/bisq/android/chart/ChartService.kt
@@ -1,4 +1,13 @@
 package bisq.android.chart
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+
 interface ChartService {
+    @GET("hloc")
+    fun getCharts(@Query("market") market: String):
+            Call<CandleResponse>
 }
