b5343429a29ba64b55568d26fd2b7ad37e75ffcb
Gabriel Prado <gaa.prado@hotmail.com>
2021-09-10 15:46:40 -0300

    Adding interval and /api to url

--- chart/src/main/java/bisq/android/chart/ChartService.kt
+++ chart/src/main/java/bisq/android/chart/ChartService.kt
@@ -1,13 +1,13 @@
 package bisq.android.chart
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 
-
 interface ChartService {
-    @GET("hloc")
-    fun getCharts(@Query("market") market: String):
-            Call<CandleResponse>
+    @GET("api/hloc")
+    suspend fun getCharts(
+        @Query("market") market: String,
+        @Query("interval") interval: String,
+    ): List<CandleResponse>
 }
