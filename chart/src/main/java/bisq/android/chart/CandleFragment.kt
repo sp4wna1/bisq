bc64a1ba45565f1435602cc782543ee476cebe08
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-22 18:24:48 -0300

    Update CandleFragment.kt

--- chart/src/main/java/bisq/android/chart/CandleFragment.kt
+++ chart/src/main/java/bisq/android/chart/CandleFragment.kt
@@ -3,6 +3,7 @@ package bisq.android.chart
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,7 @@ import androidx.core.content.ContextCompat
 import bisq.android.chart.databinding.FragmentCandleBinding
 import bisq.local.Resource
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.CandleData
-import com.github.mikephil.charting.data.CandleDataSet
-import com.github.mikephil.charting.data.CandleEntry
+import com.github.mikephil.charting.data.*
 import com.google.android.material.tabs.TabLayout
 import network.bisq.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -39,6 +38,11 @@ class CandleFragment : BaseFragment() {
 
         numberFormat.maximumFractionDigits = 0;
 
+
+        viewModel.tick.observe(viewLifecycleOwner){
+            binding.chronometer.text = it.toString()
+        }
+
         viewModel.getTickers(getPair(requireArguments()))
         viewModel.tickers.observe(viewLifecycleOwner) {
             when (it.status) {
@@ -52,6 +56,56 @@ class CandleFragment : BaseFragment() {
                     val ticker = it.data
                     val convert = numberFormat.format(BigDecimal(ticker?.last))
                     binding.valueUSD.text = convert.toString()
+
+
+                }
+            }
+        }
+
+        viewModel.volumes.observe(viewLifecycleOwner) {
+            when (it.status){
+                Resource.Status.LOADING -> {
+                    binding.progressBarChart.visibility = View.VISIBLE
+                    binding.barChart.visibility = View.INVISIBLE
+
+
+                }
+                Resource.Status.ERROR ->{
+                    binding.progressBarChart.visibility = View.INVISIBLE
+                    binding.barChart.visibility = View.INVISIBLE
+
+
+                }
+                Resource.Status.SUCCESS -> {
+                    binding.barChart.visibility = View.VISIBLE
+                    binding.progressBarChart.visibility = View.INVISIBLE
+
+
+                    val xval1 = binding.barChart.xAxis
+                    xval1.position = XAxis.XAxisPosition.BOTTOM
+                    xval1.setDrawGridLines(false)
+
+                    val volumes = it.data
+
+                    val barList = volumes?.mapIndexed { index, it -> BarEntry(index.toFloat(),it.volume.toFloat()) } ?: emptyList()
+
+
+
+                    binding.barChart.xAxis.gridColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
+                    binding.barChart.axisLeft.gridColor = ContextCompat.getColor(requireContext(), android.R.color.darker_gray)
+                    binding.barChart.axisRight.gridColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
+                    binding.barChart.xAxis.textColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
+                    binding.barChart.axisLeft.isEnabled = false
+                    binding.barChart.axisRight.textColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
+                    binding.barChart.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.background_color))
+
+                    val barDataSet = BarDataSet(barList, "teste")
+                    barDataSet.color = Color.rgb(80, 80, 80)
+                    barDataSet.valueTextColor = ContextCompat.getColor(requireContext(), android.R.color.white)
+
+
+                    binding.barChart.data = BarData(barDataSet)
+
                 }
             }
         }
@@ -60,19 +114,15 @@ class CandleFragment : BaseFragment() {
                 Resource.Status.LOADING -> {
                     binding.progressCandle.visibility = View.VISIBLE
                     binding.candleChart.visibility = View.INVISIBLE
-                    binding.barChart.visibility = View.INVISIBLE
                 }
                 Resource.Status.ERROR -> {
                     binding.progressCandle.visibility = View.INVISIBLE
                     binding.candleChart.visibility = View.INVISIBLE
-                    binding.barChart.visibility = View.INVISIBLE
-
 
                 }
                 Resource.Status.SUCCESS -> {
                     binding.progressCandle.visibility = View.INVISIBLE
                     binding.candleChart.visibility = View.VISIBLE
-                    binding.barChart.visibility = View.VISIBLE
                     binding.candleChart.invalidate()
 
                     val candles = it.data
@@ -88,19 +138,8 @@ class CandleFragment : BaseFragment() {
                         xvalue.add("8:00 PM")
                         xvalue.add("10:00 PM")
 
-//                        val xval1 = binding.barChart.xAxis
-//                        xval1.position = XAxis.XAxisPosition.BOTTOM
-//                        xval1.setDrawGridLines(false)
-//
-//                        binding.barChart.xAxis.setDrawGridLines(true)
-//                        binding.barChart.xAxis.gridColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
-//                        binding.barChart.axisLeft.gridColor = ContextCompat.getColor(requireContext(), android.R.color.darker_gray)
-//                        binding.barChart.axisRight.gridColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
-//                        binding.barChart.xAxis.textColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
-//                        binding.barChart.axisLeft.isEnabled = false
-//                        binding.barChart.axisRight.textColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
-//                        binding.barChart.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.background_color))
-//
+
+
 
 
                         val xval = binding.candleChart.xAxis
@@ -115,7 +154,6 @@ class CandleFragment : BaseFragment() {
                         binding.candleChart.axisLeft.isEnabled = false
                         binding.candleChart.axisRight.textColor = ContextCompat.getColor(requireContext(), R.color.grid_color)
                         binding.candleChart.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.background_color))
-
                         val candleList = candles.mapIndexed { index, candleResponse ->
                             CandleEntry(
                                 index.toFloat(),
@@ -137,6 +175,7 @@ class CandleFragment : BaseFragment() {
                         candleDataSet.increasingPaintStyle = Paint.Style.FILL
 
                         binding.candleChart.data = CandleData(candleDataSet)
+
                     }
                 }
             }
@@ -144,6 +183,7 @@ class CandleFragment : BaseFragment() {
         binding.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 viewModel.getCharts(getPair(requireArguments()), CandleInterval.values().get(tab?.position ?: 0))
+                viewModel.getVolumes(getPair(requireArguments()), CandleInterval.values().get(tab?.position ?: 0))
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
